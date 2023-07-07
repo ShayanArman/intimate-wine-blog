a9c595634b051f9be6d054bc86c869bf677f0d27
Shayan Arman <shayan.arman2@gmail.com>
2023-07-07 14:51:35 -0700

    simplify: logo button in Header

--- src/Components/ZeroHeader/ZeroHeader.tsx
+++ src/Components/ZeroHeader/ZeroHeader.tsx
@@ -9,145 +9,174 @@ import {
   Burger,
   rem,
   Box,
-} from '@mantine/core';
+} from "@mantine/core";
 import Image from "next/image";
 import Link from "next/link";
-import { useDisclosure } from '@mantine/hooks';
+import { useDisclosure } from "@mantine/hooks";
 import { BsChevronDown } from "react-icons/bs";
-import { FcEmptyFilter } from 'react-icons/fc'
-import classNames from 'classnames';
+import { FcEmptyFilter } from "react-icons/fc";
+import classNames from "classnames";
 
 export const HEADER_PIXEL_HEIGHT = 60;
 const HEADER_HEIGHT = rem(HEADER_PIXEL_HEIGHT);
 
-const useStyles = createStyles((theme, { backgroundColor }: {backgroundColor: string }) => ({
-  header: {
-    position: 'fixed',
-    borderBottom: 0,
-    backgroundColor: backgroundColor,
-    top: 0,
-    left: 0,
-    zIndex: 9999,
-    transition: 'background-color 0.3s ease',
-  },
-
-  headerColored: {
-    backgroundColor: "rgba(255, 255, 255, .8)",
-    transition: 'background-color 0.3s ease',
-  },
-
-
-  inner: {
-    height: HEADER_HEIGHT,
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-
-  links: {
-    [theme.fn.smallerThan('sm')]: {
-      display: 'none',
+const useStyles = createStyles(
+  (theme, { backgroundColor }: { backgroundColor: string }) => ({
+    header: {
+      position: "fixed",
+      borderBottom: 0,
+      backgroundColor: backgroundColor,
+      top: 0,
+      left: 0,
+      zIndex: 9999,
+      transition: "background-color 0.3s ease",
     },
-  },
 
-  logoDesktop: {
-    [theme.fn.smallerThan('sm')]: {
-        display: 'none',
+    headerColored: {
+      backgroundColor: "rgba(255, 255, 255, .8)",
+      transition: "background-color 0.3s ease",
     },
-  },
 
-  logoMobile: {
-    marginLeft: "1rem",
-    marginTop: '0.3rem',
-    [theme.fn.largerThan('sm')]: {
-        display: 'none',
+    inner: {
+      height: HEADER_HEIGHT,
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
     },
-  },
 
-  burger: {
-    [theme.fn.largerThan('sm')]: {
-      display: 'none',
-    },
-  },
-
-  desktopActionButton: {
-    [theme.fn.smallerThan('sm')]: {
-        display: 'none'
+    links: {
+      [theme.fn.smallerThan("sm")]: {
+        display: "none",
       },
-  },
-
-  mobileButton: {
-    [theme.fn.largerThan('sm')]: {
-        display: 'none'
-      },
-  },
-
-  link: {
-    display: 'block',
-    lineHeight: 1,
-    padding: `${rem(8)} ${rem(12)}`,
-    borderRadius: theme.radius.sm,
-    textDecoration: 'none',
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-    fontSize: theme.fontSizes.sm,
-    fontWeight: 500,
-
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
     },
-  },
 
-  linkLabel: {
-    marginRight: rem(5),
-  },
-}));
+    logo: {
+      [theme.fn.smallerThan("sm")]: {
+        marginLeft: "1rem",
+        marginTop: "0.3rem",
+      },
+    },
 
-export default function ZeroHeader({ scrolledToHeader }: { scrolledToHeader: boolean }) {
-  const { classes } = useStyles({ backgroundColor: "white"});
+    burger: {
+      [theme.fn.largerThan("sm")]: {
+        display: "none",
+      },
+    },
+
+    desktopActionButton: {
+      [theme.fn.smallerThan("sm")]: {
+        display: "none",
+      },
+    },
+
+    mobileButton: {
+      [theme.fn.largerThan("sm")]: {
+        display: "none",
+      },
+    },
+
+    link: {
+      display: "block",
+      lineHeight: 1,
+      padding: `${rem(8)} ${rem(12)}`,
+      borderRadius: theme.radius.sm,
+      textDecoration: "none",
+      color:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[0]
+          : theme.colors.gray[7],
+      fontSize: theme.fontSizes.sm,
+      fontWeight: 500,
+
+      "&:hover": {
+        backgroundColor:
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
+      },
+    },
+
+    linkLabel: {
+      marginRight: rem(5),
+    },
+  })
+);
+
+export default function ZeroHeader({
+  scrolledToHeader,
+}: {
+  scrolledToHeader: boolean;
+}) {
+  const { classes } = useStyles({ backgroundColor: "white" });
   const [opened, { toggle }] = useDisclosure(false);
 
   return (
-    <Header height={HEADER_HEIGHT} mb={120} className={classNames(classes.header, {[classes.headerColored]: scrolledToHeader})}>
+    <Header
+      height={HEADER_HEIGHT}
+      mb={120}
+      className={classNames(classes.header, {
+        [classes.headerColored]: scrolledToHeader,
+      })}
+    >
       <Container className={classes.inner} fluid>
         <Group>
-          <Burger opened={opened} onClick={toggle} className={classes.burger} size="md" color="#3DBCF8" />
-          <Link href="/" className={classes.logoDesktop}>
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            className={classes.burger}
+            size="md"
+            color="#3DBCF8"
+          />
+          <Link href="/" className={classes.logo}>
             <Image
-            width={150}
-            height={50}
-            alt="zeroInbox"
-            src="/horizWhite.svg"
+              width={150}
+              height={50}
+              alt="zeroInbox"
+              src="/horizontal.svg"
             />
           </Link>
-          <Box className={classes.logoMobile}>
-            <Link href="/">
-                <Image
-                width={150}
-                height={50}
-                alt="zeroInbox"
-                src="/horizWhite.svg"
-                />
-            </Link>
-        </Box>
           {/* <MantineLogo size={28} /> */}
         </Group>
         <Group spacing={5} className={classes.links}>
           <LinksToItems />
         </Group>
-        <Button component="a" target="_blank" href="https://app.zeroinbox.ai" leftIcon={<FcEmptyFilter />} radius="xl" size="md" color="pink" className={classes.desktopActionButton}>
+        <Button
+          component="a"
+          target="_blank"
+          href="https://app.zeroinbox.ai"
+          leftIcon={<FcEmptyFilter />}
+          radius="xl"
+          size="md"
+          color="pink"
+          className={classes.desktopActionButton}
+        >
           Click + Unsubscribe
         </Button>
         <Box className={classes.mobileButton}>
-            <Button component="a" target="_blank" href="https://app.zeroinbox.ai" leftIcon={<FcEmptyFilter />} radius="xl" size="xs" color="pink">
-                Start
-            </Button>
+          <Button
+            component="a"
+            target="_blank"
+            href="https://app.zeroinbox.ai"
+            leftIcon={<FcEmptyFilter />}
+            radius="xl"
+            size="xs"
+            color="pink"
+          >
+            Start
+          </Button>
         </Box>
       </Container>
     </Header>
   );
 }
 
-type Links = { link: string; label: string; newTab: boolean; Icon?: JSX.Element; links?: { link: string; label: string; Icon: JSX.Element, newTab: boolean }[] }[];
+type Links = {
+  link: string;
+  label: string;
+  newTab: boolean;
+  Icon?: JSX.Element;
+  links?: { link: string; label: string; Icon: JSX.Element; newTab: boolean }[];
+}[];
 
 const zeroLinks: Links = [
   {
@@ -155,7 +184,12 @@ const zeroLinks: Links = [
     label: "Privacy",
     newTab: true,
     links: [
-      { link: "/security", label: "Security", newTab: false, Icon: <FcEmptyFilter /> },
+      {
+        link: "/security",
+        label: "Security",
+        newTab: false,
+        Icon: <FcEmptyFilter />,
+      },
       {
         link: "https://zeroinbox.ai/datafaq.pdf",
         label: "Terms",
@@ -175,39 +209,55 @@ const zeroLinks: Links = [
 ];
 
 function LinksToItems({}) {
-    const { classes } = useStyles({ backgroundColor: "white" });
+  const { classes } = useStyles({ backgroundColor: "white" });
 
-    return zeroLinks.map((link) => {
-        const menuItems = link.links?.map((item) => (
-          <Menu.Item component="a" icon={item.Icon} href={item.link} key={item.label} target={item.newTab? "_blank" : "_self"}>
-            {item.label}
-          </Menu.Item>
-        ));
-    
-        if (menuItems) {
-          return (
-            <Menu key={link.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
-              <Menu.Target>
-                <a
-                  href={link.link}
-                  className={classes.link}
-                  onClick={(event) => event.preventDefault()}
-                >
-                  <Center>
-                    <span className={classes.linkLabel}>{link.label}</span>
-                    <BsChevronDown size={rem(12)} stroke={"1.5"} />
-                  </Center>
-                </a>
-              </Menu.Target>
-              <Menu.Dropdown>{menuItems}</Menu.Dropdown>
-            </Menu>
-          );
-        }
+  return zeroLinks.map((link) => {
+    const menuItems = link.links?.map((item) => (
+      <Menu.Item
+        component="a"
+        icon={item.Icon}
+        href={item.link}
+        key={item.label}
+        target={item.newTab ? "_blank" : "_self"}
+      >
+        {item.label}
+      </Menu.Item>
+    ));
 
-        return (
-          <Link href={link.link} key={link.label} target={link.newTab ? "_blank" : "_self"} className={classes.link}>
-            {link.label}
-          </Link>
-        );
-      });
+    if (menuItems) {
+      return (
+        <Menu
+          key={link.label}
+          trigger="hover"
+          transitionProps={{ exitDuration: 0 }}
+          withinPortal
+        >
+          <Menu.Target>
+            <a
+              href={link.link}
+              className={classes.link}
+              onClick={(event) => event.preventDefault()}
+            >
+              <Center>
+                <span className={classes.linkLabel}>{link.label}</span>
+                <BsChevronDown size={rem(12)} stroke={"1.5"} />
+              </Center>
+            </a>
+          </Menu.Target>
+          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
+        </Menu>
+      );
+    }
+
+    return (
+      <Link
+        href={link.link}
+        key={link.label}
+        target={link.newTab ? "_blank" : "_self"}
+        className={classes.link}
+      >
+        {link.label}
+      </Link>
+    );
+  });
 }
