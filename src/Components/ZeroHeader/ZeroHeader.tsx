de634f70b4fbc14d76820c3bac62f24e755a5b32
Shayan Arman <shayan.arman2@gmail.com>
2023-09-01 13:51:34 +0200

    new logo

--- src/Components/ZeroHeader/ZeroHeader.tsx
+++ src/Components/ZeroHeader/ZeroHeader.tsx
@@ -128,7 +128,7 @@ export default function ZeroHeader({
             color="var(--zero-blue)"
           />
           <Link href="/" style={{marginTop: "0.3rem"}}>
-            <Image width={200} height={50} alt="zeroInbox" src="/horizBlackBigger.svg" />
+            <Image width={200} height={50} alt="zeroInbox" src="/zeroInboxLogoBlack.svg" />
           </Link>
         </Flex>
         <Group spacing={5} className={classes.links}>
