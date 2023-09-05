a189d8cea1cd4df5965ed6e531b787ab4e4f1538
Shayan Arman <shayan.arman2@gmail.com>
2023-09-05 14:55:30 +0100

    navbar stuff

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,20 +1,39 @@
-import { createStyles, Flex, Button } from "@mantine/core";
+import { createStyles, Flex, Button, Box } from "@mantine/core";
 import { HEADER_HEIGHT } from "../ZeroHeader/ZeroHeader";
 
 const useStyles = createStyles(
     (theme) => ({
       container: {
-        position: "fixed", 
-        left: 0, 
-        rowGap: 5,
+        position: "fixed",
         top: HEADER_HEIGHT, 
+        left: 0,
         zIndex: 8000, 
-        backgroundColor: "var(--landing-blur)"
+        backgroundColor: "var(--landing-blur)",
+        padding: "20px",
+      },
+
+      phoneContainer: {
+        width: "100%",
+        height: "100%"
+      },
+
+      biggerContainer: {
+        marginLeft: "10px",
+        width: "20rem",
+        height: "initial",
+        backgroundColor: "var(--landing-blur)",
+        borderBottomLeftRadius: "10px",
+        borderBottomRightRadius: "10px",
+      },
+
+      content: {
+        rowGap: 5,
+        width: "100%",
       }
     })
 )
 
-export default function NavBar({opened}: { opened: boolean }) {
+export default function NavBar({opened, isSmallScreen, scrolledToHeader}: { opened: boolean, isSmallScreen: boolean, scrolledToHeader: boolean }) {
     const { classes } = useStyles();
 
     if(!opened) {
@@ -22,15 +41,15 @@ export default function NavBar({opened}: { opened: boolean }) {
     }
 
     return (
-      <Flex 
-        direction="column"
-        h={`100%`} 
-        w={"100%"} 
-        className={classes.container}> 
-        <Button>First</Button>
-        <Button>Second</Button>
-        <Button>Third</Button>
-        <Button>Fourth</Button>
+      <Flex className={`${classes.container} ${isSmallScreen ? classes.phoneContainer : classes.biggerContainer}`}>
+        <Flex 
+          direction="column"
+          className={classes.content}>
+          <Button>First</Button>
+          <Button>Second</Button>
+          <Button>Third</Button>
+          <Button>Fourth</Button>
+        </Flex>
       </Flex>
     );
 }
