359ecd240564cf8eade01a717857afbf290fa931
Shayan Arman <shayan.arman2@gmail.com>
2026-02-24 13:54:47 -0800

    create: emotion cache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '@/styles/globals.css'
 import "@/styles/variables.css"
 import Layout from '@/Components/Layout';
-import { MantineProvider } from '@mantine/core'
+import { MantineProvider, createEmotionCache } from '@mantine/core'
 import { AppProps } from 'next/app'
+
+const myCache = createEmotionCache({ key: 'mantine' })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider
+      emotionCache={myCache}
       withGlobalStyles
       withNormalizeCSS
       theme={{ colorScheme: 'light' }}
